345e93aec02233ea76d3015a6d9e934451a741e1
jlo <jlo2@csub.edu>
2024-03-25 16:01:40 -0700

    added log4j

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
 import com.google.gson.Gson;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -26,35 +29,49 @@ public class Main {
     public static final List<BuildingTypes> checkedBuildingTypes = Arrays.stream(new BuildingTypes[]{BuildingTypes.YES, BuildingTypes.HOUSE}).toList();
 
     private static List<Park> evaluatedParks;
+
     public static void main(String[] args) {
 
-        int radius = 500;
+        int radius = 250;
 
+        int count = 1;
+        System.setProperty("log4j.configurationFile", "./path_to_the_log4j2_config_file/log4j2.xml");
         Set<String> addresses = new HashSet<>();
+        NewWorkBook newWorkBook = new NewWorkBook("Z:\\workbook.xlsx");
+        Sheet mainSheet = newWorkBook.createSheet("main sheet", new String[]{"Address", "Lat", "Lon", "Ward"});
+        mainSheet.createRow(count++).createCell(0).setCellValue("//this sheet is auto-generated");
         evaluatedParks = registerParks(radius);
         for (Park x : evaluatedParks) {
             for (OverpassResponse overpassResponse : QueryProcessor.processParkQueries(x)) {
-                overpassResponse.getElements().forEach(y -> {
-                    try {
-                        String s = QueryProcessor.reverseGeocode(y.getLat(), y.getLon()).toString() + " " + x.getWard();
-                        if(addresses.add(s)) {
-                            System.out.println(s);
+                if (overpassResponse != null) {
+                    for (Element y : overpassResponse.getElements()) {
+                        try {
+                            String s = QueryProcessor.reverseGeocode(y.getLat(), y.getLon()).toString() + " " + x.getWard();
+                            if (addresses.add(s)) {
+                                Row row = mainSheet.createRow(count++);
+                                row.createCell(0).setCellValue(s);
+                                row.createCell(1).setCellValue(y.getLat());
+                                row.createCell(2).setCellValue(y.getLon());
+                                row.createCell(3).setCellValue(x.getWard().toString());
+                            }
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
                         }
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
                     }
-                });
+                }
             }
         }
+        newWorkBook.write();
     }
+
     private static List<Park> registerParks(int radius) { //gotta move this to a config this is ghetto
         List<Park> evaluatedParks = new ArrayList<>();
         evaluatedParks.add(new Park.ParkBuilder()
-                .setCoordinate(new CartesianCoordinate(35.36231,-119.04136))
+                .setCoordinate(new CartesianCoordinate(35.36231, -119.04136))
                 .setName("Saunders")
                 .setWard(Ward.WARD_2).setRadius(radius).build());
         evaluatedParks.add(new Park.ParkBuilder()
-                .setCoordinate(new CartesianCoordinate(35.37446,-119.03531))
+                .setCoordinate(new CartesianCoordinate(35.37446, -119.03531))
                 .setName("Jastro")
                 .setWard(Ward.WARD_2).build());
         return evaluatedParks;
